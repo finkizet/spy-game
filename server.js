e81717d8976264635fe6e5aca5e0e239a534454a
finkizet <143591461+finkizet@users.noreply.github.com>
2025-12-02 02:16:02 +0200

    Change server port to 10000

--- server.js
+++ server.js
@@ -297,5 +297,5 @@ function publicLobbyState(lobby) {
   };
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log('Lobby server listening on', PORT));
